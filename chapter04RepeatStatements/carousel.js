d4a7066e395cb2eb2970341ad24310a3fd3ea123
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-08-04 19:42:21 -0400

    Create carousel.js

---
+++ chapter04RepeatStatements/carousel.js
@@ -0,0 +1,10 @@
+// Write a program that launches a carousel for 10 turns, showing the turn number each time.
+for (let i = 1; i <= 10; i++) {
+	console.log(i);
+}
+// When it's done, improve it so that the number of turns is given by the user.
+function userCarousel(numOfTurns) {
+	for (let i = 1; i <= numOfTurns; i++) {
+		console.log(i);
+	}
+}
