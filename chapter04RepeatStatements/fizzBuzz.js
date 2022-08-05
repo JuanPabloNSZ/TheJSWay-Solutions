8ad5bf343c33e4e6624237160fb000eeee2ad7fd
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-08-04 20:39:26 -0400

    Update fizzBuzz.js

--- chapter04RepeatStatements/fizzBuzz.js
+++ chapter04RepeatStatements/fizzBuzz.js
@@ -4,7 +4,7 @@ It shows "Buzz" instead if the number is divisible by 5 and not by 3.
 When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
 */
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 100; i++) {
 	if (i % 3 === 0 && i % 5 === 0) {
 		console.log('FizzBuzz');
 	} else if (i % 3 === 0) {
