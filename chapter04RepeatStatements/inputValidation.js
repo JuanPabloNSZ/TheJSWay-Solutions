3b85bc16c4a140bdbdba6ed7bd694db02c0fd1a1
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-08-12 12:13:35 -0400

    Create inputValidation.js

--- chapter04RepeatStatements/inputValidation.js
+++ chapter04RepeatStatements/inputValidation.js
@@ -1,3 +1,15 @@
 // Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
+let enterNumLessOrEqual100 = function () {
+	let userNumber = window.prompt('Enter a number less than or equal to 100');
+	while (userNumber > 100) {
+		userNumber = window.prompt('Enter a number less than or equal to 100');
+	}
+};
 
 // When you are done with the above, improve the program so that the terminating number is between 50 and 100.
+let enterNumBetween50And100 = function () {
+	let userNumber = window.prompt('Enter a number between 50 and 100');
+	while (userNumber < 50 || userNumber > 100) {
+		userNumber = window.prompt('Enter a number between 50 and 100');
+	}
+};
